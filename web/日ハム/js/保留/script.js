84cfd239eaeb8547962b16dbdc13ae4c7496623a
上田慎太郎 <u.shintaro46@gmail.com>
2026-03-28 10:20:28 +0900

    Add files via upload

---
+++ web/日ハム/js/保留/script.js
@@ -0,0 +1,32 @@
+(() => {
+ document.addEventListener(`DOMContentLoaded`, () => {
+
+    const slide = document.querySelector(`.slide`);
+    const imagecarouse = document.querySelector(`.image-slide`);
+    const image = Array.from(imagecarouse.querySelectorAll(`img`));
+
+
+
+
+
+let touchStart;
+let touchMove;
+imageContainer.addEventListener(`touchstart`,(event) -> {
+   touchStart = event.touches[0].clientX;
+   touchMove = touchStart;
+});
+   imageContainer. addEventListener(
+    touchMove = event.touches[0]
+
+  } );
+  imageContainer. addEventListener(
+     if (touchMove<toString)
+      next();
+     }else if (touchMove≥toString){
+      prevz();
+     }
+  } );
+
+
+
+ });
